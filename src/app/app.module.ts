db9c0757a8008d6fe9d2dabc1a59a6bca7e8aa0c
Santiago Diaz Pace <santiagodiazpace@hotmail.com>
2020-10-26 18:39:49 -0300

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule,
