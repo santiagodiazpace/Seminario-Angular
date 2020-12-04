2d65263d1f859cd88d960e4d0bd62ef5ba1a117a
Santiago Diaz Pace <santiagodiazpace@hotmail.com>
2020-12-04 02:07:23 -0300

    Se agregaron eventos

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -44,16 +44,8 @@ export class BeerListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
-  upQuantity(beer: Beer): void {
-    if (beer.quantity < beer.stock)
-      beer.quantity++;
+  maxReached(m: string) {
+    alert(m);
   }
-
-  downQuantity(beer: Beer): void {
-    if (beer.quantity > 0)
-      beer.quantity--;
-  }
-
 
 }
